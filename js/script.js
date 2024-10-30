9f41e577c13b5d8a393d772bec433be2db30a23c
Bekarys <b6134991@gmail.com>
2024-10-30 11:19:55 +0600

    Variables and data types

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/*var name = "Amjilt Cyber School"
+
+// console.log(name);//Amjilt Cyber School
+
+var name = "Bekarys"
+
+console.log(name);//Bekarys*/
+
+
+// let name = "Amjilt Cyber School"
+
+// if (true) {
+//     let name = "Bekarys"
+//     console.log("Условияның ішіндегі аты: " + name);
+// }
+
+// console.log("Жалпы аты: " + name);
+
+
+/* 
+const name = "Bekarys"
+
+name = "Beksultan"
+
+console.log("Менің атым: " + name);
+console.log("My name is: " + name); */
+
+const name="Bekarys"
+let age //uunderfined
+let age2='10'
+
+let isBekarys=false
